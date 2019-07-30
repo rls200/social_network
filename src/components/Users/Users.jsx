d542973bf658c5581d41bfb774379cc41b493a2c
razhihan11 <razhihan11@gmail.com>
2019-07-30 21:09:55 +0300

    pagination

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,33 +1,44 @@
 import React from 'react';
 import UsersCss from './Users.module.css';
+import * as axios from 'axios';
+import avatarPhoto from '../../assets/images/personal-user-illustration-@2x.png';
 
-const Users = (props) => {
-    return <div>
-        {
-            props.users.map( u =>
-                <div key={u.id} className={UsersCss.users}>
-                    <div className={UsersCss.users__avatarFollow}>
-                        <div><img className={UsersCss.Users__avatar} src={u.avatar}/></div>
-                        <div>
-                            {u.followed ?
-                                <button onClick={ () => {props.unFollow(u.id)} } className={UsersCss.Users__button}>UNFOLLOW</button> :
-                                <button onClick={ () => {props.follow(u.id)} } className={UsersCss.Users__button}>FOLLOW</button>}
+class Users extends React.Component {
+    componentDidMount() {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            });
+    }
 
+    render() {
+        return <div>
+            {
+                this.props.users.map( u =>
+                    <div key={u.id} className={UsersCss.users}>
+                        <div className={UsersCss.users__avatarFollow}>
+                            <div><img className={UsersCss.Users__avatar} src={ u.avatar != null ? u.avatar : avatarPhoto }/></div>
+                            <div>
+                                {u.followed ?
+                                    <button onClick={ () => {this.props.unFollow(u.id)} } className={UsersCss.Users__button}>UNFOLLOW</button> :
+                                    <button onClick={ () => {this.props.follow(u.id)} } className={UsersCss.Users__button}>FOLLOW</button>}
+
+                            </div>
+                        </div>
+                        <div className={UsersCss.users__info}>
+                            <div className={UsersCss.users__info__nameStatus}>
+                                <div><p><strong>{u.name}</strong></p></div>
+                                <div><p>{u.status}</p></div>
+                            </div>
+                            <div className={UsersCss.users__info__locCountry}>
+                                <div><p>{/*u.location.sity*/}</p></div>
+                                <div><p>{/*u.location.country*/}</p></div>
+                            </div>
                         </div>
                     </div>
-                    <div className={UsersCss.users__info}>
-                        <div className={UsersCss.users__info__nameStatus}>
-                            <div><p><strong>{u.fullName}</strong></p></div>
-                            <div><p>{u.status}</p></div>
-                        </div>
-                        <div className={UsersCss.users__info__locCountry}>
-                            <div><p>{u.location.sity}</p></div>
-                            <div><p>{u.location.country}</p></div>
-                        </div>
-                    </div>
-                </div>
-            )
-        }
-    </div>
+                )
+            }
+        </div>
+    }
 }
 export default Users;
--- src/redux/users-reduser.js
+++ src/redux/users-reduser.js
@@ -2,13 +2,7 @@ const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
 
-const initialState = {
-    users: [
-        { id: 1, followed: true, fullName: 'Andrey', avatar: 'https://pp.userapi.com/c848632/v848632459/2c3d8/IIOeMFFcepg.jpg?ava=1', status: 'ereet faser bypori', location: { sity: 'Moscow', country: 'Russia' } },
-        { id: 2, followed: false, fullName: 'Sasha', avatar: 'https://pp.userapi.com/c837727/v837727327/1c6a4/-8bZakIwuZ8.jpg?ava=1', status: 'dypik losacn gyrem yojr', location: { sity: 'Moscow', country: 'Russia' } },
-        { id: 3, followed: true, fullName: 'Igor', avatar: 'https://cdn1.flamp.ru/0e6242afc92b66ebf0b054621ca2e052_100_100.jpg', status: 'killewq homitur molewosg', location: { sity: 'Moscow', country: 'Russia' } }
-    ]
-}
+const initialState = { users: [] };
 
 const usersReduser = (state = initialState, action) => {
     switch (action.type) {
